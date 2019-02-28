574fb409e38c62e33a46621caf57ec6bbca87e86
JLi <ljy5490@foxmail.com>
2019-02-28 13:56:34 +0100

    back up

--- minimal_test.py
+++ minimal_test.py
@@ -1,23 +1,22 @@
 from __future__ import print_function
-from scapy.packet import bind_layers, bind_bottom_up
-from scapy.asn1packet import ASN1_Packet
-from scapy.asn1fileds import ASN1F_INTEGER, ASN1F_IPADDRESS, ASN1F_OID, \
-        ASN1F_SEQUENCE, ASN1F_SEQUENCE_OF, ASN1F_STRING, ASN1F_TIME_TICKS, \
-        ASN1F_enum_INTEGER, ASN1F_filed, ASN1F_SEQUENCE
-from scapy.asn1.asn1 import ASN1_Class_UNIVERSAL, ASN1_Codecs, ASN1_NULL, \
-        ASN1_SEQUENCE
-from scapy.asn1.ber import BERcodec_SEQUENCE
-from scapy.sendrecv import sr1
-from scapy.volatile import RandSHort, IntAutoTIme
-from scapy.layers.inet import UDP, IP, ICMP
-
-from scapy.asn1.mib import conf
+from scapy.all import *
 
 class ASN1_Class_MMS(ASN1_Class_UNIVERSAL):
     name = "MMS"
 
-    #MmsPdu
     CONFIRMED_REQUEST_PDU = 0xa0
 
-x=ASN1_SEQUENCE([ASN1_INTEGER(7),ASN1_STRING("egg"),ASN1_SEQUENCE([ASN1_BOOLEAN(False)])])
+class ASN1_CONFIRMED_REQUEST_PDU(ASN1_SEQUENCE):
+    tag = ASN1_Class_MMS.CONFIRMED_REQUEST_PDU
+
+class BERcodec_CONFIRMED_REQUEST_PDU(BERcodec_SEQUENCE):
+    tag = ASN1_Class_MMS.CONFIRMED_REQUEST_PDU
+
+
+x=ASN1_SEQUENCE([ASN1_INTEGER(7), ASN1_STRING("egg"), ASN1_SEQUENCE([ASN1_BOOLEAN(False)])])
 x.show()
+hexdump(x)
+
+y=ASN1_CONFIRMED_REQUEST_PDU([ASN1_INTEGER(7), ASN1_STRING("egg"), ASN1_SEQUENCE([ASN1_BOOLEAN(False)])])
+y.show()
+hexdump(y)
